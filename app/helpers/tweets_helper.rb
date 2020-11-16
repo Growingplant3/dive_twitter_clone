2e8c6816a5ebdeb5dcec1dfbf0fde349e956a618
下田　佳紀 <y.s@platinum.local>
2020-11-16 14:38:54 +0900

    url遷移用のヘルパー整備

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+  def choice_path
+    if action_name == "new" || action_name == "create" || action_name == "confirm"
+      confirm_tweets_path
+    elsif action_name == "edit"
+      tweet_path
+    end
+  end
 end
